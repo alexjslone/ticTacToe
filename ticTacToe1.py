9c7f918319b9d2ff26166b7801add28c57b764f7
Alex Slone <alexjslone12@gmail.com>
2023-03-23 11:09:24 +0700

    added in function that will prompt the user to pick a square

--- ticTacToe1.py
+++ ticTacToe1.py
@@ -1,8 +1,15 @@
 import pygame
+#where I left off. Working on the bug that I got when I tried to run 
+#the font portion of the userTurn function
+#Need to figure out that bug and then check to make sure it properly shows the message
+#When someone clicks they need to remove the message.
+
+
 #since these are constant variables 
 #I'm capitalizing them
+pygame.init()
 WIDTH, HEIGHT= 400, 400 
-
+FONT= 0
 BOARD = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 square_size = 133.33
@@ -21,6 +28,7 @@ for row in range(3):
         pygame.draw.rect(BOARD, (0,0,0), (x, y, square_size, square_size))
 
 pygame.display.update()
+#creating the images
 xImage= pygame.image.load('x.png')
 oImage = pygame.image.load('o.png')
 
@@ -31,6 +39,8 @@ oImage = pygame.transform.scale(oImage, (133, 133))
 BOARD.blit(oImage, (0,0))
 BOARD.blit(xImage, (0,0))
 pygame.display.update()
+
+
 
 def main():
 
@@ -47,9 +57,47 @@ def chooseSymbol():
     #assign that symbol to the user and assign the other symbol to the computer
     xImage= pygame.image.load('x.png')
     oImage = pygame.image.load('o.png')
+    xImage= pygame.transform.scale(xImage, (133,133))
+    oImage = pygame.transform.scale(oImage, (133, 133))
     BOARD.blit(oImage, (0,0))
     BOARD.blit(xImage, (0,0))
 
+    #now I need to figure out how to assign a value to a user 
+    #ask the user to pick either x or o 
+    userSymbol = input("if you would like to be X please respond X, otherwise respond O")
+    #what is the condition for a user to be X or a user to be O? 
+    #each time they click on the board then you would fill the board with whatever they picked
+
+def userTurn():
+    #here you will want to ask the user to make a move
+    #wherever the user clicks on the board then you will fill 
+    #it in with an X or an O 
+
+    font = pygame.font.Font('Arial.ttf', 27)
+    message1 = "Make your move - click a square"
+    message_surface = font.render(message1, True, (144, 238, 144))
+    #dimensions 
+
+    message_rect = message_surface.get_rect()
+
+    #position in center of screen
+    message_rect.center = (WIDTH //2 , HEIGHT//2)
+    BOARD.blit(message_surface, message_rect)
+    pygame.display.flip()
+
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.quit()
+                exit()
+#def userClicks():
+    #run this function after showing the message to prompt the user to click 
+    #use chatGPT to figure out how to record a click
+
+
+    #now I need to figure out where the user clicks
+
+userTurn()
 
 if __name__ == "__main__":
     main()
